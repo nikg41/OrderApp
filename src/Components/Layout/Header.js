ef704970277145ffa67d18ba3a105cda0348f799
Nikhil Garg <nikhil.garg@prudential.com.sg>
2021-12-28 14:59:25 +0530

    Code added for UI

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import mealsImage from "../../assets/meals.jpg";
+import styles from "./Header.module.css";
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = props => {
+    return <Fragment>
+        <header className={styles.header}>
+            <h1>React Meals</h1>
+            <HeaderCartButton />
+        </header>
+        <div className={styles['main-image']}>
+            <img src={mealsImage} alt="A table full of food" />
+        </div>
+    </Fragment>
+}
+
+export default Header;
